d57f50abc6006752267910e7532964ce0f942af5
JyothiPavuluri <101269692+JyothiPavuluri@users.noreply.github.com>
2022-05-13 14:39:04 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -12,8 +12,8 @@
 #include <stdio.h>
 
 static void ignition_ON(void);			   // here this function will Turn on all RED LED's 
-static void status(void);		   // here this function will Turn off all the LED's at once
-static void door_status(void);  // here this function will on and off LED's with a certain delay (in anti-clockwise)
+static void wiper_ON(void);		   // here this function will Turn off all the LED's at once
+static void wiper_OFF(void);  // here this function will on and off LED's with a certain delay (in anti-clockwise)
 static void ignition_OFF(void); // here this function will on RED LED's 
 
 static void ignition_ON(void) 
@@ -29,50 +29,62 @@ static void ignition_OFF(void)
 	led_OFF(LED_RED);
 }
 
-static void car_battery_info(void) 
+static void wiper_ON(void) 
 {
 
-	led_on(LED_GREEN);
-	my_delay_ms(350);
-	led_off(LED_GREEN);
-	led_on(LED_ORANGE);
-	my_delay_ms(350);
-	led_off(LED_ORANGE);
-	led_on(LED_RED);
-	my_delay_ms(350);
-	led_off(LED_RED);
 	led_on(LED_BLUE);
-	my_delay_ms(350);
+	my_delay_ms(1000);
 	led_off(LED_BLUE);
 	led_on(LED_GREEN);
-	my_delay_ms(350);
+	my_delay_ms(1000);
 	led_off(LED_GREEN);
+	led_on(LED_ORANGE);
+	my_delay_ms(1000);
+	led_off(LED_ORANGE);
+	led_on(LED_BLUE);
+	my_delay_ms(1000);
+	led_off(LED_BLUE);
+	
 }
 
-static void door_status(void) // This function is done by Varun S with help of Nyalam Praveenraj
+static void  wiper_ON  
+{
+        led_on(LED_BLUE);
+	my_delay_ms(250);
+	led_off(LED_BLUE);
+	led_on(LED_GREEN);
+	my_delay_ms(250);
+	led_off(LED_GREEN);
+	led_on(LED_ORANGE);
+	my_delay_ms(250);
+	led_off(LED_ORANGE);
+	led_on(LED_BLUE);
+	my_delay_ms(250);
+	led_off(LED_BLUE);
+	
+}
+ static void wiper_ON(void) 
 {
 
-	led_on(LED_GREEN);
-	my_delay_ms(350);
-	led_off(LED_GREEN);
 	led_on(LED_BLUE);
-	my_delay_ms(350);
+	my_delay_ms(1000);
 	led_off(LED_BLUE);
-	led_on(LED_RED);
-	my_delay_ms(350);
-	led_off(LED_RED);
-	led_on(LED_ORANGE);
-	my_delay_ms(350);
-	led_off(LED_ORANGE);
 	led_on(LED_GREEN);
-	my_delay_ms(350);
+	my_delay_ms(1000);
 	led_off(LED_GREEN);
+	led_on(LED_ORANGE);
+	my_delay_ms(1000);
+	led_off(LED_ORANGE);
+	led_on(LED_BLUE);
+	my_delay_ms(1000);
+	led_off(LED_BLUE);
+	
 }
 
 
 int main(void)
 
-
+{
 	led_init_all();
 	button_init();
 
@@ -91,14 +103,20 @@ int main(void)
 		}
 		else if (btncnt == 3)
 		{ // comparing the no of counts of button
-			car_battery_info();
-
-			btncnt = 0;
+			 wiper_ON();
+			 btncnt = 0;
 		}
 		else if (btncnt == 4)
 		{ // comparing the no of counts of button
-			door_status();
-			btncnt = 0;
+		         wiper_ON();
+			 btncnt = 0;
+		}
+		else if (btncnt == 5)
+		{ // comparing the no of counts of button
+		         wiper_OFF();
+			 btncnt = 0;
+			
 		}
 	}
+
 }
