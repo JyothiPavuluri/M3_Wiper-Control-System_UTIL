4858048615e0e36371962e08fcc6e66d5d68c975
JyothiPavuluri <101269692+JyothiPavuluri@users.noreply.github.com>
2022-05-14 13:59:28 +0530

    Update Header.h

--- 3_Implementation/Inc/Header.h
+++ 3_Implementation/Inc/Header.h
@@ -1,12 +1,3 @@
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <errno.h>
+
+#include "own_drivers_and_func.h"
 #include <stdio.h>
-#include <signal.h>
-#include <time.h>
-#include <sys/time.h>
-#include <sys/times.h>
-
-
-#include <errno.h>
-#include <stdint.h>
